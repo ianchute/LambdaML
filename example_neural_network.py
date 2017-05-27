006eb82acef6e78f79080564a15f0b6b154ae80d
Ian Chu Te <ianchute@hotmail.com>
2017-05-27 23:43:28 +0800

    Change example to ReLU unit.

--- example_neural_network.py
+++ example_neural_network.py
@@ -7,20 +7,27 @@ def get_data():
     circles = pd.read_csv('data/circles.csv')
     return circles[['x','y']].values, circles['label'].values
 
-def tanh_neuron(x,p,w_key,b_key):
-    """Neuron with tanh activation."""
+def neuron(x,p,w_key,b_key):
+    """Neuron with ReLU activation."""
     signal = p[w_key].dot(x) + p[b_key]
-    return np.tanh(signal)
+    return np.max([0, signal])
 
-def hidden_layer(x, p):
+def hidden_layer_1(x, p):
     return [
-        tanh_neuron(x,p,'w1','b1'),
-        tanh_neuron(x,p,'w2','b2'),
+        neuron(x,p,'w1','b1'),
+        neuron(x,p,'w2','b2'),
+    ]
+
+def hidden_layer_2(x, p):
+    return [
+        neuron(x,p,'w3','b3'),
+        neuron(x,p,'w4','b4'),
     ]
 
 def neural_network(x, p):
-    activations = hidden_layer(x,p)
-    signal = p['wf'].dot(activations) + p['bf']
+    activations_1 = hidden_layer_1(x,p)
+    activations_2 = hidden_layer_2(activations_1,p)
+    signal = p['wf'].dot(activations_2) + p['bf']
     return (np.tanh(signal) + 1) / 2
 
 X,Y = get_data()
@@ -31,6 +38,12 @@ p = {
     'b1': np.random.uniform(),
     'w2': np.array([np.random.uniform() for i in range(len(X[0]))]),
     'b2': np.random.uniform(),
+
+    'w3': np.array([np.random.uniform() for i in range(len(X[0]))]),
+    'b3': np.random.uniform(),
+    'w4': np.array([np.random.uniform() for i in range(len(X[0]))]),
+    'b4': np.random.uniform(),
+
     'wf': np.array([np.random.uniform() for i in range(len(X[0]))]),
     'bf': np.random.uniform(),
 }
